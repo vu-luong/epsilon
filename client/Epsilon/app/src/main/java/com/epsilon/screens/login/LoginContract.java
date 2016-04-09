5508e2459f34705269a23d3e83ab1994ede4a597
dandoh <dainhan605@gmail.com>
2016-04-09 11:13:00 +0700

    Add interface contract login

--- client/Epsilon/app/src/main/java/com/epsilon/screens/login/LoginContract.java
+++ client/Epsilon/app/src/main/java/com/epsilon/screens/login/LoginContract.java
@@ -1,7 +1,22 @@
 package com.epsilon.screens.login;
 
+import com.epsilon.commons.GenericViewInterface;
+
 /**
  * Created by Dandoh on 4/9/16.
  */
 public interface LoginContract {
+
+
+    interface View extends GenericViewInterface {
+        void displayErrorUsername();
+        void displayErrorPassword();
+        void goToRegisterScreen();
+        void displayLoginSucceed();
+    }
+
+    interface UserActionListener {
+        void login(String username, String password);
+        void toRegister();
+    }
 }
