aaabee6144f859ba6f7a7d5c0a7842eaaaaa9bd4
dandoh <dainhan605@gmail.com>
2016-04-09 11:54:02 +0700

    set up register screen, add mock version

---
+++ client/Epsilon/app/src/main/java/com/epsilon/screens/register/RegisterFragment.java
@@ -0,0 +1,34 @@
+package com.epsilon.screens.register;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epsilon.R;
+import com.epsilon.commons.GenericRetainedFragment;
+
+/**
+ * Created by Dandoh on 4/9/16.
+ */
+public class RegisterFragment extends GenericRetainedFragment{
+
+    public static Fragment getInstance() {
+        return new RegisterFragment();
+    }
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ client/Epsilon/app/src/main/java/com/epsilon/screens/register/RegisterPresenter.java
@@ -0,0 +1,7 @@
+package com.epsilon.screens.register;
+
+/**
+ * Created by Dandoh on 4/9/16.
+ */
+public class RegisterPresenter implements RegisterContract.UserActionListener {
+}
